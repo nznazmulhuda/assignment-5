9e060d1f53d7fcd54b3e87c82756006ebf424e00
Nazmul Huda <nznazmulhdua04@gmail.com>
2024-02-18 16:22:34 +0600

    main section done

--- js/index.js
+++ js/index.js
@@ -1,1 +1,59 @@
-console.log("hello world");
+const seats = document.querySelectorAll(".seat");
+let seatCount = 0;
+let seatField = document.getElementById("seat");
+const seatClass = "Economoy";
+const ticketPrice = 550;
+for (const seat of seats) {
+	seat.addEventListener("click", function () {
+		let seatName = document.createElement("p");
+		let seatclass = document.createElement("p");
+		let price = document.createElement("p");
+		if (seat.classList.contains("bg-theme_btn")) {
+			seat.classList.remove("bg-theme_btn", "text-white");
+			seatName.classList.remove("seatName");
+			seatName.setAttribute("id", "toRemove");
+		} else {
+			seat.classList.add("bg-theme_btn", "text-white");
+			seatName.classList.add("seatName", "text-left");
+		}
+		seatCount = document.querySelectorAll(".bg-theme_btn").length;
+
+		if (seatCount > 4) {
+			alert("You can not buy more then 4 seats");
+			seat.classList.remove("bg-theme_btn");
+			seatCount = 4;
+		}
+
+		document.getElementById("seatCount").innerText = seatCount;
+
+		document.getElementById("seatLeft").innerText = 40 - seatCount;
+
+		seatclass.classList.add("text-center");
+		price.classList.add("text-right");
+		seatclass.innerText = seatClass;
+		price.innerText = ticketPrice;
+
+		if (seatName.classList.contains("seatName")) {
+			seatName.innerText = seat.innerText;
+			seatField.appendChild(seatName);
+			seatField.appendChild(seatclass);
+			seatField.appendChild(price);
+		} else if (
+			seatName.classList.contains(".toRemove") &&
+			seatField.getElementById("toRemove").innerText == seatName
+		) {
+			document.getElementById("toRemove").innerText = "";
+		}
+
+		document.getElementById("price").innerText = seatCount * ticketPrice;
+		document.getElementById("grandTotalPrice").innerText =
+			seatCount * ticketPrice;
+
+		if (seatCount > 0) {
+			document.getElementById("couponField").classList.remove("hidden");
+		} else {
+			document.getElementById("couponField").classList.add("hidden");
+			document.getElementById("discount").classList.add("hidden");
+		}
+	});
+}
--- js/utils.js
+++ js/utils.js
@@ -1,1 +1,52 @@
-console.log("hello world");
+function coupon() {
+	const couponOne = "NEW15";
+	const couponTwo = "Couple 20";
+	const coupon = document.getElementById("coupon").value;
+
+	if (coupon === couponOne) {
+		document.getElementById("couponField").classList.add("hidden");
+		document.getElementById("discount").classList.remove("hidden");
+		document.getElementById("discount").classList.add("flex");
+
+		document.getElementById("discountPrice").innerText =
+			ticketPrice * seatCount * 0.15;
+
+		document.getElementById("grandTotalPrice").innerText =
+			ticketPrice * seatCount - ticketPrice * seatCount * 0.15;
+	} else if (coupon === couponTwo) {
+		document.getElementById("couponField").classList.add("hidden");
+		document.getElementById("discount").classList.remove("hidden");
+		document.getElementById("discount").classList.add("flex");
+
+		document.getElementById("discountPrice").innerText =
+			ticketPrice * seatCount * 0.2;
+
+		document.getElementById("grandTotalPrice").innerText =
+			ticketPrice * seatCount - ticketPrice * seatCount * 0.2;
+	} else {
+		alert("invalid coupon or insert your number");
+		document.getElementById("coupon").value = "";
+	}
+}
+
+function submit() {
+	if (
+		document.getElementById("number").value &&
+		document.getElementById("pName").value
+	) {
+		document.getElementById("header").classList.add("hidden");
+		document.getElementById("ticketSell").classList.add("hidden");
+		document.getElementById("offer").classList.add("hidden");
+		document.getElementById("body").classList.add("bg-[#F2F1F1]");
+		my_modal_1.showModal();
+	} else {
+		alert("Enter name and valid number");
+	}
+}
+
+function unHide() {
+	document.getElementById("header").classList.remove("hidden");
+	document.getElementById("ticketSell").classList.remove("hidden");
+	document.getElementById("offer").classList.remove("hidden");
+	document.getElementById("body").classList.remove("bg-[#F2F1F1]");
+}
